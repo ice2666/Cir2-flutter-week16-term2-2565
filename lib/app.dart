b019980a1a36f21653870138d9483ce241858713
rathapoom260448 <rathaphumiwicitrsmbati.890@gmail.com>
2023-01-31 13:53:31 +0700

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home.dart';
+import 'screens/signin.dart';
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SignIn(),
+    );
+  }
+}
